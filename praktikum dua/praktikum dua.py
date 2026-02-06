4eaa0c2a1ee05c01bc5be207868e989277d373f1
yasminkhoirunnisa2007-source <yasminkhoirunnisa.2007@gmail.com>
2026-02-06 20:55:22 +0700

    Initial commit

---
+++ praktikum dua/praktikum dua.py
@@ -0,0 +1,155 @@
+#=======================================================
+# Praktikum 2: Konsep ADT dan File Handing(STUDI KASUS)
+# Latihan 1: Membuat Fungsi Load Data dari File
+#=======================================================
+
+#variabel menyimpan data file
+nama_file = "data.txt"
+
+def baca_data(nama_file):
+    data_dict = {} #inisialisasi data dictionary
+    with open(nama_file, "r", encoding="utf-8") as file:
+        for baris in file:
+            baris = baris.strip()
+            nim, nama, nilai = baris.split(",") #ambil data per item data
+            data_dict[nim]= {"nama": nama, "nilai": int(nilai)}
+        return data_dict 
+    
+buka_data = baca_data(nama_file) 
+print ("jumlah data terbaca", len(buka_data))
+
+
+#=======================================================
+# Praktikum 2: Konsep ADT dan File Handing(STUDI KASUS)
+# Latihan 2: Membuat Fungsi Menampilkan Data
+#=======================================================
+
+def tampilkan_data(data_dict):
+    #Membuat header tabel
+    print("\n=== DAFTAR MAHASISWA ===")
+    print(f"{'NIM' : <10} | {'Nama': <12}  | {'Nilai' :>5}")
+    '''
+    untuk tampilan yang rapi, atur lebar kolom
+    {'NIM': <10} artinya nim rata kiri dengan lebar kolom 10 karakter
+    {'Nama': <12} artinya nama rata kiri dengan lebar kolom 12 karakter
+    {'Nilai' :>5} artinya nilai rata kanan dengan lebar kolom 5 karakter
+    '''
+    print("-"*32) #Membuat garis
+
+    #Menampilkan isi datanya
+    for nim in sorted(data_dict.keys()):
+        nama = data_dict[nim]["nama"]
+        nilai = data_dict[nim]["nilai"]
+        print(f"{nim:<10} | {nama:<12} | {int(nilai):<5}")
+
+tampilkan_data(buka_data) #Memanggil fungsi untuk menampilkan data
+
+
+#=======================================================
+# Praktikum 2: Konsep ADT dan File Handing(STUDI KASUS)
+# Latihan 3: Membuat Fungsi Mencari data
+#=======================================================
+
+def cari_data(data_dict):
+    #pencarian data berdasarkan nim sebagai key dictionary
+    #membuat input nim mahasiswa yang akan dicari
+    nim_cari = input("masukkan NIM mahasiswa yang ingin dicari: ".strip())
+
+    if nim_cari in data_dict:
+        nama= data_dict[nim_cari]["nama"]
+        nilai = data_dict[nim_cari]["nilai"]
+
+        print("==== Data Mahasiswa Ditemukan ====")
+        print(f"NIM  : {nim_cari}")
+        print(f"Nama : {nama}")
+        print(f"Nilai :{nilai}")
+    else:
+        print("Data tidak ditemukan. Pastikan NIM yang dimasukan benar")
+
+    #memanggil fungsi cari data
+    cari_data(buka_data)
+    
+
+#=======================================================
+# Praktikum 2: Konsep ADT dan File Handing(STUDI KASUS)
+# Latihan 4: Membuat Fungsi Update Data
+#=======================================================
+#membuat fungsi update data
+def ubah_data(data_dict):
+    #Awali dgn mencari nim/data mahasiswa yang ingin di update
+    nim = input("Masukkan NIM Mahasiswa yang ingin diubah datanya")
+
+    if nim not in data_dict:
+        print("NIM tidak ditemukan. Update dibatalkan")
+        return
+    try:
+        nilai_baru = int(input("Masukkan nilai dari 0-100: ")).strip()
+    except ValueError:
+        print("Nilai harus berupa angka. Update dibatalkan")
+        return 
+    
+    if nilai_baru < 0 or nilai_baru >100:
+        print("Nilai harus antara 0 sampai 100. Update dibatalkan")
+        return
+    nilai_lama = data_dict[nim]["nilai"]
+    data_dict[nim]["nilai"] = nilai_baru
+
+    print(f"Update berhasil. Nilai {nim} berubah dari {nilai_lama} menjadi nilai baru")
+    ubah_data(buka_data)
+
+#=======================================================
+# Praktikum 2: Konsep ADT dan File Handing(STUDI KASUS)
+# Latihan 5: Membuat Fungsi Menyimpan Data pada File
+#=======================================================
+
+#membuat fungsi menyimpan data ke file
+def simpan_data(nama_file, data_dict):
+    with open(nama_file, "w", encoding="utf-8") as file:
+        for nim in sorted(data_dict.keys()):
+            nama = data_dict[nim]["nim"]
+            nilai = data_dict[nilai]["nilai"]
+            file.write(f"{nim}, {nama,}, {nilai}\n")
+
+        simpan_data(nama_file, buka_data)
+        print("\nData Berhasil Disimpan Ke File :", nama_file)
+
+#=======================================================
+# Praktikum 2: Konsep ADT dan File Handing(STUDI KASUS)
+# Latihan 6: Membuat Menu Interaktif
+#=======================================================
+
+def main():
+    #load data otomatis saat program dimulai
+    buka_data = baca_data(nama_file)
+
+    while True:
+        print("\n=== MENU DATA MAHASISWA===")
+        print("1. Tampilkan Data Mahasiswa")
+        print("2. Cari Data Berdasarkan NIM")
+        print("3. Ubah Nilai Mahasiswa")
+        print("4. Simpan Data ke File")
+        print("0. Keluar")
+
+        pilihan = input("Pilih Menu: ").strip()
+
+        if pilihan == "1":
+            tampilkan_data(buka_data) #memanggil fs 2 menampilkan data
+
+        elif pilihan == "2":            #memanggil fs.3 mencari data
+            cari_data(buka_data)
+
+        elif pilihan == "3":             #memanggil fs.4 mengubah data
+            ubah_data(buka_data)
+
+        elif pilihan == "4" :                    #memanggil fs.5 menyimpan data ke file
+            simpan_data(nama_file, buka_data)
+    
+        elif pilihan == "0":
+            print("Program Selesai.")
+            break
+
+        else:
+            print("kesalahan")
+
+if __name__ == "__main__":
+    main()
---
+++ praktikum satu/praktikum 1.py
@@ -0,0 +1,72 @@
+#---------------------------------------------------------------------------
+#Praktikum 1 : Konsep ADT dan File Handling
+#Latihan Dasar 1 : Membaca seluruh isi file data
+#---------------------------------------------------------------------------
+
+print("---- Membuka file dalam satu satu string ----")
+with open("data_mahasiswa.txt", "r", encoding="utf-8") as file:
+    isi_file = file.read()
+print(isi_file)
+
+print("Tipe Data :", type(isi_file))
+
+print("===== Membuka file per baris =====")
+jumlah_baris = 0
+with open("data_mahasiswa.txt", "r", encoding="utf-8") as file:
+    for baris in file:
+        jumlah_baris = jumlah_baris + 1
+        baris = baris.strip() # Menghilangkan karakter garis baru
+        print(f"Baris ke-{jumlah_baris}")
+        print(f"Isinya: {baris}")
+
+#============================================================
+# Praktikum 1 Konsep ADT dan File Handling 
+# Latihan dasar 2 Parsing baris menjadi data satuan dan menampilkan dalam bentuk kolom data 
+#============================================================
+
+
+with open('data_mahasiswa.txt', 'r', encoding='utf-8') as file:
+    for baris in file:
+        baris = baris.strip() #menghilangkan karakter baris baru
+        nim, nama, nilai = baris.split(",") #pecah data variabel satuan dan simpan ke 
+        print("NIM  :", nim, "| Nama : ", nama, "| Nilai :", nilai) #menampilkan satuan data dalam bentuk kolom 
+       
+
+#---------------------------------------------------------------------------
+#Praktikum 1 : Konsep ADT dan File Handling
+#Latihan Dasar 1 : Membaca Data dan Menyimpannya ke Struktur Data List
+#---------------------------------------------------------------------------
+
+# Parsing baris menjadi data satuan dan menampilkannya dalam bentuk kolom data
+data_list = [] #inisialisasi list untuk menampung data
+
+with open("data_mahasiswa.txt", "r", encoding="utf-8") as file:
+    for baris in file:
+        baris = baris.strip()
+        nim, nama, nilai = baris.split(",")
+        print(f"Nim: {nim} | Nama: {nama} | Nilai: {nilai}")
+        data_list.append([nim, nama, int(nilai)])
+
+print("===== Menampilkan List =====")
+print(data_list)
+print("Contoh record ke-1", data_list[0])
+print("Contoh record ke-1", data_list[1])
+print("Jumlah record pada list", len(data_list))
+
+#============================================================
+# Praktikum 1 Konsep ADT dan File Handling 
+# Latihan 4 membaca data dan menyimpannya ke dalam struktur data dictionary 
+#============================================================
+
+
+data_dict = {} #inisialisasi dictionary 
+with open('data_mahasiswa.txt', 'r', encoding='utf-8') as file:
+    for baris in file:
+        baris = baris.strip() #menghilangkan karakter baris baru
+        nim, nama, nilai = baris.split(",") #pecah data variabel satuan dan simpan ke 
+        data_dict[nim] = {
+            'nama': nama,
+            'nilai': int(nilai)
+        } 
+print("====Menampilkan Data Dictionary====")
+print(data_dict)
